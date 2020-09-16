3ce98c2afce5f29d3901ac5e66720c8aa99f6efb
Marc Hein <dev@marc-hein.de>
2020-09-16 11:52:39 +0200

    first commit

--- NFCTest/ViewController.swift
+++ NFCTest/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+            print(error.localizedDescription)
+        
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for record in message.records {
+                if let string = String(data: record.payload, encoding: .ascii) {
+                    print(string)
+                }
+            }
+        }
+    }
+    
+    
+    var session: NFCNDEFReaderSession?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        session = NFCNDEFReaderSession(delegate: self, queue: DispatchQueue.main, invalidateAfterFirstRead: false)
+        session?.begin()
+        
+        print(session)
     }
 
 
